c451578d8b25bdcfe595b4862f310e63be322e68
elaizabautista <2023-2-02500@lpunetwork.edu.ph>
2026-05-18 20:24:22 +0800

    aa

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace PreciseSkin___LUMYVUE
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ImageIngestForm());
+            Application.Run(new WelcomeForm());
         }
     }
 }
