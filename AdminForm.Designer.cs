62668715dbd8c67f5f8580c7f32515cfb33a6c47
elaizabautista <2023-2-02500@lpunetwork.edu.ph>
2026-05-19 22:39:32 +0800

    aa

--- AdminForm.Designer.cs
+++ AdminForm.Designer.cs
@@ -28,7 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            PatientIDHistoryTxt = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            dateTimePicker2 = new DateTimePicker();
+            SearchBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic Light", 72F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.SaddleBrown;
+            label1.Location = new Point(86, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(1073, 124);
+            label1.TabIndex = 0;
+            label1.Text = "PreciseSkin - LUMYVUE";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.MistyRose;
+            dataGridView1.BorderStyle = BorderStyle.Fixed3D;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(86, 285);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1425, 509);
+            dataGridView1.TabIndex = 1;
+            // 
+            // PatientIDHistoryTxt
+            // 
+            PatientIDHistoryTxt.Location = new Point(161, 258);
+            PatientIDHistoryTxt.Name = "PatientIDHistoryTxt";
+            PatientIDHistoryTxt.Size = new Size(480, 23);
+            PatientIDHistoryTxt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(92, 264);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Patient ID:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(656, 263);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Date:";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(698, 256);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 5;
+            dateTimePicker2.ValueChanged += dateTimePicker2_ValueChanged;
+            // 
+            // SearchBtn
+            // 
+            SearchBtn.Location = new Point(904, 256);
+            SearchBtn.Name = "SearchBtn";
+            SearchBtn.Size = new Size(121, 23);
+            SearchBtn.TabIndex = 6;
+            SearchBtn.Text = "button1";
+            SearchBtn.UseVisualStyleBackColor = true;
+            SearchBtn.Click += SearchBtn_Click;
             // 
             // AdminForm
             // 
@@ -36,12 +108,32 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.PeachPuff;
             ClientSize = new Size(1564, 850);
+            Controls.Add(SearchBtn);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(PatientIDHistoryTxt);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
             Name = "AdminForm";
             Text = "AdminForm";
+            WindowState = FormWindowState.Maximized;
             Load += AdminForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        public Label label1;
+        public DataGridView dataGridView1;
+        public TextBox PatientIDHistoryTxt;
+        public Label label2;
+        public Label label3;
+        public DateTimePicker dateTimePicker2;
+        public Button SearchBtn;
+        private TextBox textBox1;
     }
 }
--- AdminForm.cs
+++ AdminForm.cs
@@ -1,23 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Text;
+using Microsoft.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace PreciseSkin___LUMYVUE
 {
     public partial class AdminForm : Form
     {
+        private string connectionString =
+            @"Data Source=LAPTOP-0DMT6OS6\SQLEXPRESS;
+              Initial Catalog=PreciseSkinDB;
+              Integrated Security=True;
+              TrustServerCertificate=True";
+
         public AdminForm()
         {
             InitializeComponent();
         }
 
+        // LOAD ALL RECORDS WHEN FORM OPENS
         private void AdminForm_Load(object sender, EventArgs e)
         {
+            LoadAllPatients();
+        }
 
+        private void LoadAllPatients()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlDataAdapter da =
+                    new SqlDataAdapter("SELECT * FROM Patients", conn);
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        // SEARCH BUTTON (BY PATIENT ID)
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            string searchValue = PatientIDHistoryTxt.Text.Trim();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"
+                    SELECT * FROM Patients
+                    WHERE PatientId LIKE @PatientId";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@PatientId", "%" + searchValue + "%");
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        // CLICK ROW TO VIEW DETAILS
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                PatientIDHistoryTxt.Text = row.Cells["PatientId"].Value.ToString();
+            }
+        }
+
+        // OPTIONAL: FILTER BY DATE
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"
+                    SELECT * FROM Patients
+                    WHERE ConsultationDate LIKE @Date";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Date", "%" + dateTimePicker2.Value.ToString("yyyy-MM-dd") + "%");
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+            }
         }
     }
 }
